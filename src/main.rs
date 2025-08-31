5d769e940fc6d6708ee0e7f93496679c5b9cff42
revellan <climb.simonschmidt@web.de>
2025-08-31 10:35:42 +0200

    Finish beta version

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,21 @@ fn main() {
     argparse.add_argument("--load", Some("-l"), "Load the page", None);
     argparse.run();
     let start_process = match argparse.get("--start-process") {
-        ArgState::False => "Arg wasn't called".to_string(),
-        ArgState::True => "Arg was called".to_string(),
-        ArgState::Value(s) => s,
+        ArgState::False => "wasn't called".to_string(),
+        ArgState::True => panic!("Impossible!"),
+        ArgState::Value(s) => format!("is {}",s),
     };
-    println!("{}", start_process)
+    println!("--start-process {}", start_process);
+    let reload = match argparse.get("--reload") {
+        ArgState::False => "wasn't called".to_string(),
+        ArgState::True => "was called".to_string(),
+        ArgState::Value(_) => panic!("Impossible!"),
+    };
+    println!("--reload {}", reload);
+    let load = match argparse.get("--load") {
+        ArgState::False => "wasn't called".to_string(),
+        ArgState::True => "was called".to_string(),
+        ArgState::Value(_) => panic!("Impossible!"),
+    };
+    println!("--load {}", load);
 }
